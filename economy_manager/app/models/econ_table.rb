66e5945fb9a87c41098d8096b56779410ba721cb
Spartwo <sparthesecond@gmail.com>
2024-12-19 10:37:57 +0000

    pre-merger

--- economy_manager/app/models/econ_table.rb
+++ economy_manager/app/models/econ_table.rb
@@ -1,2 +1,11 @@
 class EconTable < ApplicationRecord
+  def calculated_deficit
+    divisor = status == "Thrifty" ? 4.0 : 5.0
+    gross_val = gross || 0
+    trade_val = trade || 0
+    expenditure_val = expenditure || 0
+    reserve_val = reserve || 0
+
+    -(((reserve_val + (gross_val / divisor) + trade_val) - (expenditure_val.abs**1.01 + reserve_val)) / 2.0)
+  end
 end
--- economy_manager/app/views/econ_tables/_econ_table.json.jbuilder
+++ economy_manager/app/views/econ_tables/_econ_table.json.jbuilder
@@ -1,2 +1,3 @@
 json.extract! econ_table, :id, :acronym, :name, :imgUrl, :gross, :trade, :expenditure, :reserve, :status, :co, :iso, :ant, :state, :ind, :created_at, :updated_at
 json.url econ_table_url(econ_table, format: :json)
+
